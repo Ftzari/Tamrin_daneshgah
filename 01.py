25ccbfd8833b712112f8e07e7278e9169b6ed2b9
Ftzari <121855642+Ftzari@users.noreply.github.com>
2023-01-11 20:40:54 +0330

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,9 @@
+n = int(input("Input Number N: "))
+for i in range(1, n):
+    print(i*"*")
+print(n*"*")
+for i in range(n-1, 0,-1):
+    print(i*"*")
+    #fatemeh tazari
+#Sunday.      7:45  ~  10
+
---
+++ revers.py
@@ -0,0 +1,9 @@
+#fatemeh tazari
+#Sunday.      7:45  ~  10
+
+def my_function(x):
+       return x[::-1]
+x=input("enter a word:")
+mytxt = my_function(x)
+
+print(mytxt)
