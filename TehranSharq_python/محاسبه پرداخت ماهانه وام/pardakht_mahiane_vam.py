406101deb8ea759bdcdaeb0b64d79b9e9dbaaea3
Ftzari <121855642+Ftzari@users.noreply.github.com>
2023-01-11 20:53:46 +0330

    Add files via upload

---
+++ TehranSharq_python/محاسبه پرداخت ماهانه وام/pardakht_mahiane_vam.py
@@ -0,0 +1,20 @@
+def calc_monthly_payment(p: float, r: float, d: int):
+    try:
+        r = (r / 100)
+        monthly_payment = p * (r*pow(1+r, d))
+        monthly_payment = (monthly_payment / pow(1+r, d)-1)/12
+
+    except ZeroDivisionError:
+        n = d * 12
+        monthly_payment = p / n
+    return monthly_payment
+
+
+p = float(input("Input Principal: "))
+r = float(input("Input Rate: "))
+d = float(input("Input Duration: "))
+print(calc_monthly_payment(p, r, d))
+
+    #fatemeh tazari
+#Sunday.      7:45  ~  10
+
